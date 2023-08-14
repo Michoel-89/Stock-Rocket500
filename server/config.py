67c439da3daa52371988c14344f14229bd152dcb
michoel <michoelr111@gmail.com>
2023-08-14 16:04:09 -0400

    npm build and update requirements.txt

--- server/config.py
+++ server/config.py
@@ -9,7 +9,7 @@ import os
 # Local imports
 
 # Instantiate app, set attributes
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='../client/build', template_folder='../client/build')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
